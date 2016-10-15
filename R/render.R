25a5201978a80f9840a8fb3ae3cf8897f935bce4
Jordan S Read <jread@usgs.gov>
2016-10-14 20:44:17 -0500

    fixing docs

--- R/render.R
+++ R/render.R
@@ -7,33 +7,33 @@
 #' @param filename name of file to write metadata to
 #' @param \dots additional lists to include or other arguments passed to methods
 #' (e.g., \code{template="metadata.mustache"})
+#' @param template a character or filepath
+#' @details template can be missing, a filepath to a *.mustache file, or a string
+#' @keywords internal
+#' @examples
+#' render(list('dogname'='fred','catname'='midred'),
+#'    filename=NULL, list('dogname'='betty'), template="my dog's name is: {{dogname}}")
+#'
+#' @seealso \code{\link[whisker]{whisker.render}}
 #'
 #' @export
 render <- function(data, filename, ...){
   UseMethod("render")
 }
 
+#' @describeIn render render text to a file from a yaml file
 #' @importFrom yaml yaml.load_file
 #' @export
-render.character <- function(data, filename, ...){
+render.character <- function(data, filename, ..., template){
   stopifnot(file.exists(data))
   config.text <- yaml::yaml.load_file(data)
   render(data = config.text, filename = filename, ...)
 }
 
-#' render from a list
-#'
-#' render text to a file from a list
-#'
-#' @describeIn render
-#' @param template a character or filepath
-#' @details template can be missing, a filepath to a *.mustache file, or a string
-#' @importFrom whisker whisker.render
-#' @keywords internal
-#' @examples
-#' render(list('dogname'='fred','catname'='midred'),
-#'    filename=NULL, list('dogname'='betty'), template="my dog's name is: {{dogname}}")
+
+#' @describeIn render render text to a file from a list
 #' @export
+#' @importFrom whisker whisker.render
 render.list <- function(data, filename, ..., template){
   if (missing(template)){
     template <- system.file(package=packageName(),'extdata', "FGDC_template.mustache")
