199e9e0e04a2c18f26450eebf95b84e4e0e6b279
Jordan S Read <jread@usgs.gov>
2016-10-19 14:28:03 -0500

    attr_names functions and docs

--- R/read_data.R
+++ R/read_data.R
@@ -1,19 +1,45 @@
-
+#' read data from supported file formats
+#'
+#' read in data from filepath into R
+#'
+#' @param filename a complete file path
+#' @param \dots additional arguments passed to file-specific methods.
+#' @export
 read_data <- function(filename, ...){
   UseMethod("read_data")
+}
+
+#' @export
+#' @keywords internal
+read_data.character <- function(filename, ...){
+  stopifnot(file.exists(filename))
+  class(filename) <- get_filetype(filename)
+  UseMethod("read_data", object = filename)
 }
 
 #' @importFrom rgdal readOGR
 #' @importFrom tools file_path_sans_ext
 #' @importFrom utils read.table write.table
+#' @export
+#' @keywords internal
 read_data.shapefile <- function(filename, ...){
   rgdal::readOGR(dirname(filename[1]), layer = tools::file_path_sans_ext(filename[1]), ...)
 }
 
+#' @export
+#' @keywords internal
+read_data.shapedir <- function(filename, ...){
+  rgdal::readOGR(filename, layer = tools::file_path_sans_ext(dir(filename)[1]), ...)
+}
+
+#' @export
+#' @keywords internal
 read_data.csvfile <- function(filename, ...){
   read.table(filename, header=TRUE, sep=',', stringsAsFactors = FALSE, ...)
 }
 
+#' @export
+#' @keywords internal
 read_data.tsvfile <- function(filename, ...){
   read.table(filename, header=TRUE, sep='\t', stringsAsFactors = FALSE, ...)
 }
@@ -25,7 +51,7 @@ get_filetype <- function(filename){
     if (dir.exists(filename) && 'shp' %in% tools::file_ext(dir(filename))){
       type <- 'shapedir'
     } else {
-      type <- paste0(tools::file_ext(filename),'file')
+      type <- paste0(tools::file_ext(filename), 'file')
     }
 
   } else {
